8b04c95cd8e51457b1dbfe3f0635fc753eb856ec
Bayartsengel Battulga <tsengelbattulga1227@gmail.com>
2021-07-21 16:24:51 -0700

    Completed Function Lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let calcBlocks = (Math.abs (blocks - 42));
+    return calcBlocks;
+}
+function distanceFromHqInFeet(blocks) {
+    let calcBlocks = (Math.abs (blocks - 42));
+    let calcFeet = (calcBlocks * 264)
+    return calcFeet;    
+}
+function distanceTravelledInFeet(a, b) {
+    let travelFeet = (Math.abs (a - b)* 264);
+    return travelFeet;
+}
+function calculatesFarePrice(a, b) {
+    let farePrice = distanceTravelledInFeet (a, b)
+        if (farePrice < 400) {
+            return 0;
+        } else if (farePrice > 400 && farePrice < 2000) {
+            return (farePrice - 400) * 0.02;
+        } else if (farePrice > 2001 && farePrice < 2500) {
+            return 25;
+        } else if (farePrice > 2500) {
+            return 'cannot travel that far'
+        }
+
+}
+
+
+
+
